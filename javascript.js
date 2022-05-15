bf0d595d8ed27f6424b2ee6d1424f2f952689dbc
terryjung0244 <terryjung0244@gmail.com>
2022-05-15 09:19:07 -0400

    map updated

--- javascript.js
+++ javascript.js
@@ -118,16 +118,32 @@ const userData = {
   },
 };
 
-// userTerry - location - movingHistory 0번째 인덱스 값을 뽑아라.
-// userTerry - location - movingHistory 각 값을 뽑아라.
-// userTerry - location - movingHistory 에서 from이 Seoul인 address를 뽑아라.
-// userSungjun의 relationship을 확인해서 각 다른 user의 데이터를 뽑아라.
-// 해당 데이터의 location - movingHistory 0번째 인덱스의 from 데이터를 뽑아라.
-// 새로운 object를 만들고, 해당 object안에 userTerry의 value를 넣어라.
-// 새로운 object를 만들고, 해당 object안에 userTerry의 location - movingHistory 배열을 넣어라.
+// 1.userTerry - location - movingHistory 0번째 인덱스 값을 뽑아라.
+// 2.userTerry - location - movingHistory 각 값을 뽑아라.
+// 3.userTerry - location - movingHistory 에서 from이 Seoul인 address를 뽑아라.
+// 4.userSungjun의 relationship을 확인해서 각 다른 user의 데이터를 뽑아라.
+// 5.해당 데이터의 location - movingHistory 0번째 인덱스의 from 데이터를 뽑아라.
+// 6.새로운 object를 만들고, 해당 object안에 userTerry의 value를 넣어라.
+// 7.새로운 object를 만들고, 해당 object안에 userTerry의 location - movingHistory 배열을 넣어라.
 
-console.log(userData.userTom.location.movingHistory[1].to.province)
-console.log(userData.userTom.location.movingHistory[1]['to']['province'])
+// console.log(userData.userTerry.location.movingHistory[0])
+
+// userData.userTerry.location.movingHistory.map((element) => {
+//   return element[1]
+// })
+
+// console.log(userData.userTerry.location.movingHistory[1]['from']['address'])
+
+
+
+
+
+
+
+
+
+
+
 
 
 // console.log(userData[userData['userTerry']['relationship'][0]]);
